771d399f5b6f282d0381171a60187c1b09d6cde4
undeadtokenart <undeadtokenart@gmail.com>
2023-03-14 20:30:29 -0700

    added look for placeholder function

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
-def word_replacer(key, placeholder, document):
-    with open(document, "r") as f:
-        doc = f.read()
-    words = doc.split()
+import re
+
+
+# opens a document in read mode
+def opendoc(path):
+    with open(path, "r") as f:
+        return f.read()
+
+# word replacement function, takes word to replace with, placeholder to change and path of document
+def word_replacer(key, placeholder, path):
+        doc = opendoc(path)
+        words = doc.split()
+        for i in range(len(words)):
+            if words[i] == placeholder:
+                words[i] = key
+        return " ".join(words)
+
+# finds a placeholder word in a string and returns a list of all words found
+def lookup(path):
+    doc = opendoc(path)
+    return re.findall(r'\[[^\]]*\]', doc)
+
+'''    words = doc.split()
+    list = []
+
     for i in range(len(words)):
-        if words[i] == placeholder:
-            words[i] = key
-    return " ".join(words)
+        matches = re.findall(r'\[[^\]]*\]', doc)
+        if words[i] == matches:
+             print(words[i])
+             list.append(words[i])
+
+    return list
+'''
 
 
-print(word_replacer("Stephen","[[First_Name]]", "template.txt"))
+print(lookup("template.txt"))
+             
+
+
+#function that looks through a document or folder and gets all placeholders
+#function that makes a list of placeholder words and make sure there are no duplicates
+#function that makes a spread sheet that will have placeholders in one column
+#function that saves the document
+#function that combines first and last names into one string
+#function that connects to google drive?????
+#function that prompts for matching the replacment word with the placeholder
+#function that asks if there are more than one set of names to be used in the contract
