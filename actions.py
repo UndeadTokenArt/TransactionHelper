15d54d990c3f61bd479d95e0b0467848a0c419da
undeadtokenart <undeadtokenart@gmail.com>
2023-03-28 16:51:06 -0700

    Some attempts at connecting a working function to the front end

--- actions.py
+++ actions.py
@@ -1,16 +1,29 @@
 import re
 import gspread
-from flask import Flask
+from google.oauth2.service_account import Credentials
 
-app = Flask(__name__)
+'''
+Code used for connecting the google account Oauth stuff _ not ready yet 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+# Load credentials from the JSON key file
+creds = Credentials.from_service_account_file('path/to/key.json')
+
+# Authenticate with gspread
+client = gspread.authorize(creds)
+
+# Open the spreadsheet by name
+sheet = client.open('Spreadsheet Name').sheet1
+'''
 
 def start_gspread():
     gc = gspread.service_account()
-    wks = gc.open("WordReplacemenmt").sheet1
+    try:
+        wks = gc.open("WordReplacemenmt").sheet1
+    except:
+        gc.create('WordReplacemenmt')
+        raise Exception("No sheet found, new one created")
+        
 
 # opens a document in read mode
 def opendoc(path):
@@ -18,7 +31,7 @@ def opendoc(path):
         return f.read()
 
 # word replacement function, takes word to replace with, placeholder to change and path of document
-def word_replacer(key, placeholder, path):
+def word_replacer(key="Berry Cool", placeholder="[Your Name]", path="template.txt"):
         doc = opendoc(path)
         words = doc.split()
         for i in range(len(words)):
--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, request, flash
+from main import word_replacer
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html") 
+    return render_template("index.html")
+
+@app.route("/result/<key>", methods=['POST', 'GET'])
+def test(key):
+    text = word_replacer(key)
+    return f"<p>{text}</p>"
+
+@app.route("/textreplacer")
+def textreplacer():
+    return render_template("form.html")
